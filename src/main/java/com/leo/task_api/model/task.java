48284bd4f4a8d1c270d2e92ea4ed64f036959448
Leonardo250 <leomoulin2002@gmail.com>
2026-01-15 00:58:12 -0300

    criação da classe task em andamento

---
+++ src/main/java/com/leo/task_api/model/task.java
@@ -0,0 +1,24 @@
+package com.leo.task_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class task  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private  String descriptionn;
+    private String status;
+
+    @CreationTimestamp // notação para criar a data
+    private LocalDateTime createdAt;
+
+}
