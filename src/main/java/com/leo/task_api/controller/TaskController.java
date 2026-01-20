133e228856640c53c1216f33201894c59ed5430b
Leonardo250 <leomoulin2002@gmail.com>
2026-01-20 18:50:17 -0300

    funções de buscar todos, buscar por id, salvar e deletar conculidos conexão entre service e controller estabelecida

--- src/main/java/com/leo/task_api/controller/TaskController.java
+++ src/main/java/com/leo/task_api/controller/TaskController.java
@@ -1,8 +1,12 @@
 package com.leo.task_api.controller;
 
 import com.leo.task_api.Service.TaskService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.leo.task_api.model.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -10,9 +14,29 @@ public class TaskController {
 
     private final TaskService taskService;
 
+
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Task>> findAll(){
+        return ResponseEntity.ok(taskService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id){
+        return ResponseEntity.ok(taskService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> save(@RequestBody Task task){
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.save(task));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return ResponseEntity.noContent().build();
+    }
 
 }
